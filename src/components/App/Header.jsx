c17e1bbdf41c8928667a9b4586cc06f198653003
chesterlm <loaizamazocristian@gmail.com>
2023-10-09 13:20:38 -0500

    icon modification and added image

--- src/components/App/Header.jsx
+++ src/components/App/Header.jsx
@@ -1,23 +1,24 @@
-// /src/components/Header.js
-
 import React from 'react';
-import { FaPlane, FaGlobe, FaBell, FaUser } from 'react-icons/fa';
 import './Header.scss';
+import logo from '../../assets/icons/logo.svg'; // Importa el logo SVG
+import globeIcon from '../../assets/icons/location.svg'; // Importa el icono de ubicación
+import bellIcon from '../../assets/icons/notifications.svg'; // Importa el icono de notificaciones
+import avatarIcon from '../../assets/images/Rectangle.png'; // Importa la imagen de avatar
 
 const Header = () => {
     return (
         <header className="header">
-        <div className="header-logo">
-            <FaPlane className="plane-icon" />
-            <span className="logo-text">E-flight</span>
-        </div>
-        <div className="header-center-space"></div>
-        <div className="header-currency">
-            <span className="currency-text">USD</span>
-            <FaGlobe className="globe-icon" />
-            <FaBell className="bell-icon" />
-            <FaUser className="avatar-icon" />
-        </div>
+            <div className="header-logo">
+                <img src={logo} alt="Logo" className="plane-icon" />
+                <span className="logo-text">E-flight</span>
+            </div>
+            <div className="header-center-space"></div>
+            <div className="header-currency">
+                <span className="currency-text">USD</span>
+                <img src={globeIcon} alt="Globe" className="globe-icon" />
+                <img src={bellIcon} alt="Bell" className="bell-icon" />
+                <img src={avatarIcon} alt="Avatar" className="avatar-icon" /> {/* Utiliza la variable "avatarIcon" para mostrar la imagen */}
+            </div>
         </header>
     );
 };
