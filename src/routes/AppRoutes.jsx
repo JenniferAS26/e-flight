4b5c488a44e0d5e5b66582ae32d22cecec2d7486
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-09 20:49:46 -0500

    SortBy component styles and structure, mobile version

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '../components/Layout'
 import Home from '../pages/Home'
+import FlightListing from '../pages/FlightListing'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path='flight-listing' element={<FlightListing />} />
       </Route>
     </Routes>
   )
