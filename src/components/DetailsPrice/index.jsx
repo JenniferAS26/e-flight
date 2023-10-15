900c5b2dbaee31edf33e8083b075589296628adc
kelly saldaña <kellysalb@hotmail.com>
2023-10-15 14:44:07 -0500

    agregando estilos

--- src/components/DetailsPrice/index.jsx
+++ src/components/DetailsPrice/index.jsx
@@ -11,29 +11,29 @@ const DetailsPrice = () => {
   return (
    <section className='Price-Houston-container-major'>
     <div className="Details-Pice-major">
-        <div className='Deatails-header-Price'>
-            <div className='Deatail-Title'>
-                <span>$</span>
-                <h1>Price details</h1>
-                <img src={ImegeLogo} alt="logo" />
-                <img src={ImegePrice} alt="precio" />
-                <img className='lineahorizontal' src={ImegeLine9} alt="linea" />
+        <div className='deatail-container-box'>
+            <div className='Deatails-header-Price'>
+                <div className='Deatail-Title'>
+                    <img src={ImegePrice} alt="precio" />
+                    <h1>Price details</h1>
+                    <img className='lineahorizontal' src={ImegeLine9} alt="linea" />
+                </div>
             </div>
-        </div>
-        <div className="Details-price__wrapper">
-            <div className="Datails-basic">
-                <p className="Adult">adult basic fee</p>
-                <p className="cien">$150</p>
-                <p className="tax">Tax</p>
-                <p className="Regulaqr">Regular total price</p>
-                <p className="ciento-cincuenta">$150</p>
-                <p className="Save">Save</p>
-                <p className="uno-punto">$1.5</p>
-                <img className='lineahorizontal' src={ImegeLine9} alt="linea" />
-            </div>
-            <div className="Details-total">
-                <p className="total">Total</p>
-                <p className="price">$148.5</p>
+            <div className="Details-price__wrapper">
+                <div className="Datails-basic">
+                    <p className="Adult">adult basic fee</p>
+                    <p className="cien">$150</p>
+                    <p className="tax">Tax</p>
+                    <p className="Regulaqr">Regular total price</p>
+                    <p className="ciento-cincuenta">$150</p>
+                    <p className="Save">Save</p>
+                    <p className="uno-punto">$1.5</p>
+                    <img className='lineahorizontal' src={ImegeLine9} alt="linea" />
+                </div>
+                <div className="Details-total">
+                    <p className="total">Total</p>
+                    <p className="price">$148.5</p>
+                </div>
             </div>
         </div>
     </div>
@@ -41,7 +41,7 @@ const DetailsPrice = () => {
         <div className="houston-header">
             <div className="houston-tittle">
                 <h1>Houston-Los Ageles</h1>
-                <img src={ImegeAirline} alt="avion" />
+                <img src={ImegeLogo} alt="logo" />
                 <img className='lineahorizontal' src={ImegeLine9} alt="linea" />
             </div>
             <div className="houston-container__wrapper">
@@ -51,7 +51,7 @@ const DetailsPrice = () => {
                     <span>HOU - LAS</span>
                     <div className="economy">
                         <p>economy</p>
-                        <img src={ImegeLandingAir} alt="avion" />
+                        <img src={ImegeAirline} alt="avion" />
                     </div>
                    </div>
                     <div className="container-nueve">
