90cd6a52d2138b3c6f8fd501f9b370c1d17af305
kelly saldaña <kellysalb@hotmail.com>
2023-10-15 16:33:14 -0500

    organizando el index

--- src/components/DetailsPrice/index.jsx
+++ src/components/DetailsPrice/index.jsx
@@ -12,113 +12,112 @@ import './styles.scss'
 const DetailsPrice = () => {
   return (
    <section className='Price-Houston-container-major'>
-    <div className="Details-Pice-major">
-            <div className='Details-header-Price'>
-                <div className='Deatail-Title'>
-                    <img src={ImegePrice} alt="precio" />
-                    <h1>Price details</h1>
+        <div className="Details-Pice-major">
+                <div className='Details-header-Price'>
+                    <div className='Deatail-Title'>
+                        <img src={ImegePrice} alt="precio" />
+                        <h1>Price details</h1>
+                    </div>
                 </div>
-            </div>
-           <div className='lineahorizontal'> <img  src={ImegeLine9} alt="linea" /></div>
-            <div className="Details-price__wrapper">
-                <div className="Details-basic">
-                    <div className="Adult"><p >Adult basic fee</p></div>
-                    <div className="cien"><p >$150</p></div>
-                    <div  className="tax"><p>Tax</p></div>
-                    <div className="Regular"><p >Regular total price</p></div>
-                    <div className="ciento-cincuenta"><p >$150</p></div>
-                    <div className="Save"><p >Save</p></div>
-                    <div className="uno-punto"><p>-$1.5</p></div>
+            <div className='lineahorizontal'> <img  src={ImegeLine9} alt="linea" /></div>
+                <div className="Details-price__wrapper">
+                    <div className="Details-basic">
+                        <div className="Adult"><p >Adult basic fee</p></div>
+                        <div className="cien"><p >$150</p></div>
+                        <div  className="tax"><p>Tax</p></div>
+                        <div className="Regular"><p >Regular total price</p></div>
+                        <div className="ciento-cincuenta"><p >$150</p></div>
+                        <div className="Save"><p >Save</p></div>
+                        <div className="uno-punto"><p>-$1.5</p></div>
+                    </div>
+                    <div><img className='lineahorizontal' src={ImegeLine9} alt="linea" /></div>
+                    <div className="Details-total">
+                        <div className="total"><p >Total</p></div>
+                        <div className="price"><p >$148.5</p></div>
+                    </div>
+                </div>
+        </div>
+        <div className="Huoston-container-major">
+            <div className="houston-header">
+                <div className="houston-tittle">
+                    <h1>Houston-Los Ageles</h1>
+                    <img src={ImegeLogo} alt="logo" />
                 </div>
                 <div><img className='lineahorizontal' src={ImegeLine9} alt="linea" /></div>
-                <div className="Details-total">
-                    <div className="total"><p >Total</p></div>
-                    <div className="price"><p >$148.5</p></div>
-                </div>
-            </div>
-    </div>
-    <div className="Huoston-container-major">
-        <div className="houston-header">
-            <div className="houston-tittle">
-                <h1>Houston-Los Ageles</h1>
-                <img src={ImegeLogo} alt="logo" />
-                <img className='lineahorizontal' src={ImegeLine9} alt="linea" />
-            </div>
-            <div className="houston-container__wrapper">
-                <div className="Houston-cloudy-container-1">
-                   <div className="houston-airlines">
-                    <h1>Coludy Airlines</h1>
-                    <span>HOU - LAS</span>
-                    <div className="economy">
-                        <p>economy</p>
-                        <img src={ImegeAirline} alt="avion" />
+                <div className="houston-container__wrapper">
+                    <div className="Houston-cloudy-container-1">
+                    <div className="houston-airlines">
+                        <h1>Coludy Airlines</h1>
+                        <span>HOU - LAS</span>
+                        <div className="economy">
+                            <p>economy</p>
+                            <img src={ImegeAirline} alt="avion" />
+                        </div>
                     </div>
-                   </div>
-                    <div className="container-nueve">
-                        <span>9</span>
-                        <p>Feb, 2023</p>
+                        <div className="container-nueve">
+                            <span>9</span>
+                            <p>Feb, 2023</p>
+                        </div>
+                        <div className="houston-hours">
+                            <h1>23:15 - 1:25</h1>
+                            <img src={ImegeTime} alt="ciculo" />
+                            <h2>2h10m</h2>
+                        </div>
+                        <div className="direct">
+                            <img src={ImegeGroup9} alt="lineapunto" />
+                            <p>Direct</p>
+                            <img src={ImegeLandinAir2 } alt="avion" />
+                        </div>
                     </div>
-                    <div className="houston-hours">
-                        <h1>23:15 - 1:25</h1>
-                        <img src={ImegeTime} alt="ciculo" />
-                        <h2>2h10m</h2>
+                    <div className='linea-horizontal'><img  src={ImegeLine9} alt="linea" /></div>
+                    <div className="houston-Refundable-container-1">
+                        <div className="refundable">
+                            <span>$</span>
+                            <p>Refundable</p>
+                        </div>
+                        <div className="reschedule">
+                            <img src={ImegeCalendar} alt="calendario" />
+                            <p>Reschedule Available</p>
+                        </div>
                     </div>
-                    <div className="direct">
-                        <img src={ImegeGroup9} alt="lineapunto" />
-                        <p>Direct</p>
-                        <img src={ImegeLandinAir2 } alt="avion" />
+                    <div className="Houston-cloudy-container-2">
+                    <div className="houston-airlines">
+                        <h1>Coludy Airlines</h1>
+                        <span>LAS -LAX</span>
+                        <div className="economy">
+                            <p>economy</p>
+                            <img src={ImegeLandingAir} alt="avion" />
+                        </div>
                     </div>
-                </div>
-                <div className='linea-horizontal'><img  src={ImegeLine9} alt="linea" /></div>
-                <div className="houston-Refundable-container-1">
-                    <div className="refundable">
-                        <span>$</span>
-                        <p>Refundable</p>
+                        <div className="container-nueve">
+                            <span>10</span>
+                            <p>Feb, 2023</p>
+                        </div>
+                        <div className="houston-hours">
+                            <h1>2:25 - 7:40</h1>
+                            <img src={ImegeTime} alt="ciculo" />
+                            <h2>5h15m</h2>
+                        </div>
+                        <div className="direct">
+                            <img src={ImegeGroup9} alt="lineapunto" />
+                            <p>Direct</p>
+                            <img src={ImegeLandinAir2 } alt="avion" />
+                        </div>
                     </div>
-                    <div className="reschedule">
-                        <img src={ImegeCalendar} alt="calendario" />
-                        <p>Reschedule Available</p>
-                    </div>
-                </div>
-                <div className="Houston-cloudy-container-2">
-                   <div className="houston-airlines">
-                    <h1>Coludy Airlines</h1>
-                    <span>LAS -LAX</span>
-                    <div className="economy">
-                        <p>economy</p>
-                        <img src={ImegeLandingAir} alt="avion" />
-                    </div>
-                   </div>
-                    <div className="container-nueve">
-                        <span>10</span>
-                        <p>Feb, 2023</p>
-                    </div>
-                    <div className="houston-hours">
-                        <h1>2:25 - 7:40</h1>
-                        <img src={ImegeTime} alt="ciculo" />
-                        <h2>5h15m</h2>
-                    </div>
-                    <div className="direct">
-                        <img src={ImegeGroup9} alt="lineapunto" />
-                        <p>Direct</p>
-                        <img src={ImegeLandinAir2 } alt="avion" />
-                    </div>
-                </div>
-                <div className='linea-horizontal'><img  src={ImegeLine9} alt="linea" /></div>
-                <div className="houston-Refundable-container-2">
-                    <div className="refundable">
-                        <span>$</span>
-                        <p>Refundable</p>
-                    </div>
-                    <div className="reschedule">
-                        <img src={ImegeCalendar} alt="calendario" />
-                        <p>Reschedule Available</p>
+                    <div className='linea-horizontal'><img  src={ImegeLine9} alt="linea" /></div>
+                    <div className="houston-Refundable-container-2">
+                        <div className="refundable">
+                            <span>$</span>
+                            <p>Refundable</p>
+                        </div>
+                        <div className="reschedule">
+                            <img src={ImegeCalendar} alt="calendario" />
+                            <p>Reschedule Available</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
    </section>
 
   )
