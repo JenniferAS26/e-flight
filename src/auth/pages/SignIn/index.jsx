49c64cd1846b439658a71635336695834285851f
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-23 08:09:42 -0500

    SignIn and SignUp styles complete

--- src/auth/pages/SignIn/index.jsx
+++ src/auth/pages/SignIn/index.jsx
@@ -2,7 +2,9 @@ import { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
 import { getDataByParams } from '../../../services/usersService'
+import logo from '../../../assets/icons/logo.svg'
 import Swal from 'sweetalert2'
+import './styles.scss'
 
 const SignIn = () => {
   // const { register, handleSubmit } = useForm()
@@ -51,29 +53,45 @@ const SignIn = () => {
   }
 
   return (
-    <section>
-      <h2>Ingresa tu usuario</h2>
+    <section className='sign-form'>
       <form
-        className='grid'
+        className='sign-form__form'
         onSubmit={ handleSubmit }
       >
-        <label htmlFor='email-signin'>Ingresa tu Email</label>
-        <input 
-          type='email' 
-          id='email-signin' 
-          name='email' 
-          value={email}
-          onChange={ event => setEmail(event.target.value) }
-        />
-        <label htmlFor='password-signin'>Ingresa tu contraseña</label>
-        <input 
-          type='password' 
-          id='password-signin' 
-          name='password' 
-          value={password}
-          onChange={ event => setPassword(event.target.value) }
-        />
-        <input type="submit" value='Iniciar sesión' />
+        <div className='sign-form__form--logo-container'>
+          <img src={logo} alt="logo icon" />
+          <span>E-flight</span>
+        </div>
+        <h2 className='sign-form__form--title'>Ingresa tu usuario</h2>
+        <p className='sign-form__form--text'>Si ya eres parte de E-flight, ingresa tus datos</p>
+        <div className="sign-form__form--field-wrapper">
+          <input
+            type='email'
+            id='email-signin'
+            name='email'
+            value={email}
+            onChange={ event => setEmail(event.target.value) }
+          />
+          <label htmlFor='email-signin'>Ingresa tu Email</label>
+        </div>
+        <div className="sign-form__form--field-wrapper">
+          <input
+            type='password'
+            id='password-signin'
+            name='password'
+            value={password}
+            onChange={ event => setPassword(event.target.value) }
+          />
+          <label htmlFor='password-signin'>Ingresa tu contraseña</label>
+        </div>
+        <input className='sign-form__form--button' type="submit" value='Iniciar sesión' />
+        <button 
+          className='sign-form__form--button-sec' 
+          type="button"
+          onClick={() => navigate('/sign-up')}
+        >
+          Crear cuenta
+        </button>
       </form>
       
     </section>
