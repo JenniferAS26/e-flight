74f5521446a3c263e41a041d880fa485e0bb4560
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-22 22:28:28 -0500

    Auto re-new token

--- src/services/flightService.js
+++ src/services/flightService.js
@@ -1,19 +1,31 @@
 import axios from 'axios'
 import endpoints from './endpoints'
-const ACCESS_TOKEN = 'Nzb8JA0lBwMUYqAqzA1iBBPhVpeu'
+import { getToken } from './auth'
 
-const getListOfFlights = async (origin, destination, departureDate, passengers, stops) => {
-  const config = {
-    headers: {
-      "content-type": "application/vnd.amadeus+json",
-      "Authorization": `Bearer ${ACCESS_TOKEN}`
+const getListOfFlights = async (origin, destination, departureDate, passengers, stops, classes) => {
+  let retries = 0
+  while( retries < 3 ){
+    try {
+      let ACCESS_TOKEN = localStorage.getItem('authToken')
+      // console.log(ACCESS_TOKEN)
+      const config = {
+        headers: {
+          "content-type": "application/vnd.amadeus+json",
+          "Authorization": `Bearer ${ACCESS_TOKEN}`
+        }
+      }
+      const { data } = await axios.get(endpoints.searchByDestination(origin, destination, departureDate, passengers, stops, classes), config)
+      return data.data
+    } catch (error) {
+      // console.warn(error.response)
+      if (error.response.status == 401) {
+        const { access_token } = await getToken()
+        localStorage.setItem('authToken', access_token)
+        retries = retries + 1
+      } else {
+        retries = retries + 1
+      }
     }
-  }
-  try {
-    const { data } = await axios.get(endpoints.searchByDestination(origin, destination, departureDate, passengers, stops), config)
-    return data.data
-  } catch (error) {
-    console.warn(error)
   }
 }
 
