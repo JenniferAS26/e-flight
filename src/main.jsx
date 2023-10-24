247bf13d68d17c4f0bc74637855965b6fd213c89
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-24 01:06:27 -0500

    add basename in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import AuthProvider from './auth/context/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/e-flight-app.netlify.app/'>
       <AuthProvider>
         <FlightSearchProvider>
           <AppRoutes />
