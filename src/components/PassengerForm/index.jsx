087b8a175f424860081b1d72cc4091163d8d36da
chesterlm <loaizamazocristian@gmail.com>
2023-10-24 00:04:24 -0500

    update

--- src/components/PassengerForm/index.jsx
+++ src/components/PassengerForm/index.jsx
@@ -9,81 +9,81 @@ class PassengerForm extends React.Component {
         return (
             <div className="App">
                 <div className="section">
-                    <img src={alertIcon} alt="Alert Icon" />
-                    <h2>Passenger details</h2>
-                    <p>Name as on ID card/passport without title and punctuation</p>
-                    <form>
+                    <img className="img" src={alertIcon} alt="Alert Icon" />
+                    <h2 className="h2">Passenger details</h2>
+                    <p className='parrafo'>Name as on ID card/passport without title and punctuation</p>
+                    <form className='form'>
                         <div className="input-container">
-                            <label>Name</label>
-                            <input className="name" type="text" />
+                            <label className='labael'>Name</label >
+                            <input  className="name" type="text" />
                         </div>
                         <div className="input-container">
-                            <label>Surname</label>
-                            <input type="text" />
+                            <label className='labael'>Surname</label >
+                            <input className='input' type="text" />
                         </div>
                         <div className="input-container">
-                            <label>Title</label>
-                            <input type="text" />
+                            <label className='labael'>Title</label >
+                            <input className='input' type="text" />
                         </div>
                         <div className="input-container">
-                            <label>Birthday</label>
-                            <input type="date" />
+                            <label className='labael'>Birthday</label >
+                            <input className='input' type="date" />
                         </div>
                         <div className="input-container">
-                            <label>Nationality</label>
-                            <input type="text" />
-                        </div>
-                    </form>
-                </div>
-
-                <div className="section">
-                    <img src={idIcon} alt="ID Icon" />
-                    <h2>Identity</h2>
-                    <p>Passport valid for at least 6 months from departure date is required for international travel or transit abroad</p>
-                    <form>
-                        <div className="input-container">
-                            <label>Passport number</label>
-                            <input type="text" />
-                        </div>
-                        <div className="input-container">
-                            <label>Country of Issue</label>
-                            <input type="text" />
-                        </div>
-                        <div className="input-container">
-                            <label>Passport Expiry Date</label>
-                            <input type="date" />
+                            <label className='labael'>Nationality</label >
+                            <input className='input' type="text" />
                         </div>
                     </form>
                 </div>
 
                 <div className="section">
-                    <img src={userIcon} alt="User Icon" />
-                    <h2>Contact details</h2>
+                    <img className="img" src={idIcon} alt="ID Icon" />
+                    <h2 className="h2">Identity</h2>
+                    <p className='parrafo'> Passport valid for at least 6 months from departure date is required for international travel or transit abroad</p>
                     <form>
                         <div className="input-container">
-                            <label>Name</label>
-                            <input type="text" />
+                            <label className='labael'>Passport number</label >
+                            <input className='input' type="text" />
                         </div>
                         <div className="input-container">
-                            <label>Surname</label>
-                            <input type="text" />
+                            <label className='labael'>Country of Issue</label >
+                            <input className='input' type="text" />
                         </div>
                         <div className="input-container">
-                            <label>Title</label>
-                            <input type="text" />
-                        </div>
-                        <div className="input-container">
-                            <label>Email</label>
-                            <input type="email" />
-                        </div>
-                        <div className="input-container">
-                            <label>Phone number</label>
-                            <input type="tel" />
+                            <label className='labael'>Passport Expiry Date</label >
+                            <input className='input' type="date" />
                         </div>
                     </form>
                 </div>
 
-                <button>Submit</button>
+                <div className="section">
+                    <img className="img" src={userIcon} alt="User Icon" />
+                    <h2 className="h2">Contact details</h2>
+                    <form>
+                        <div className="input-container">
+                            <label className='labael'>Name</label >
+                            <input className='input' type="text" />
+                        </div>
+                        <div className="input-container">
+                            <label className='labael'>Surname</label >
+                            <input className='input' type="text" />
+                        </div>
+                        <div className="input-container">
+                            <label className='labael'>Title</label >
+                            <input className='input' type="text" />
+                        </div>
+                        <div className="input-container">
+                            <label className='labael'>Email</label >
+                            <input className='input' type="email" />
+                        </div>
+                        <div className="input-container">
+                            <label className='labael'>Phone number</label >
+                            <input className='input' type="tel" />
+                        </div>
+                    </form>
+                </div>
+
+                <button className='boton'>Submit</button>
             </div>
         );
     }
